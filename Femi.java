ae41e5b3dd4758b436402bfeb917ad4ee3e3249c
Macwinner1 <ifechukwuw@gmail.com>
2025-05-04 17:42:19 +0100

    second commit of functions to display prcls

--- Femi.java
+++ Femi.java
@@ -5,7 +5,21 @@ public static int findMultiple(int number){
 return number * 5;
 }
 
+public static void displayPrimeNumbers(){
 
+for(int index = 2; index <= 50; index++){
+	int count = 0;
+for(int index2 = 1; index2 <= index; index2++){
+	if(index % index2 == 0){
+		count++;
+	}
+}
+	if(count == 2){
+		System.out.println(index + " ");
+	}
+}
+
+}
 
 
 
