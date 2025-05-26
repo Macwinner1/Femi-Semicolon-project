3485e69dc9289bdf59a8556d9a8255ab5aec0744
Macwinner1 <ifechukwuw@gmail.com>
2025-05-26 17:29:59 +0100

    first gate files

--- First Gate/ArraySquares.java
+++ First Gate/ArraySquares.java
@@ -1,10 +1,27 @@
 import java.util.Arrays;
 public class ArraySquares{
+
 public static void main(String[] args){
 
-int[] numbers = {9, 2, 7, 0, 5};
+int[] numbers = {9, 7, 1, 5, 2};
+int[] result = new int[5];
+result[0] = 100000;
+int[] temp = new int[1];
 
-System.out.print(Arrays.toString(ArraySquaresFunction.squaresAscending(numbers)));
+for(int index = 0; index < numbers.length; index++){
+	for(int item = index; item < numbers.length; item++){
+	if(result[index] > numbers[item]){
+		result[index] = numbers[item];
+	}
+	else if(numbers[item] > result[index]){
+	result[index] = numbers[item];
+	}
+	
+
+	}
+System.out.print(Arrays.toString(result));
+}
 
 }
+
 }
--- First Gate/ArraySquaresFunction.java
+++ First Gate/ArraySquaresFunction.java
@@ -1,23 +1,24 @@
 public class ArraySquaresFunction{
 
-public static int[] squaresAscending(int[] numbers){
+public static int[] arraySquaresAscending(int[] numbers){
 
-int[] result = new int[5]; 
+int[] result = new int[5];
+result[0] = 100000;
+int[] temp = new int[1];
 
 for(int index = 0; index < numbers.length; index++){
-for(int item = index; item < numbers.length; item++){
-if(numbers[index] > numbers[item]){
-result[index] = numbers[item];
-else if(numbers[item] > numbers[index]){
-result[index] = numbers[index];
-}
-}
+	for(int item = index; item < numbers.length; item++){
+	if(result[index] > numbers[item]){
+		result[index] = numbers[item];
+	}
+	else if(numbers[item] > result[index]){
+	result[index] = numbers[item];
+	}
+	
 
-}
-
-}
-
+	}
 return result;
+
 }
 
 }
